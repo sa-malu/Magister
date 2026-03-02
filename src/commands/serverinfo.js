e397eec30682b230726b5e9a7c0454e305c36190
sa_malu <68854747+sa-malu@users.noreply.github.com>
2026-03-01 23:04:25 -0300

    Update serverinfo.js

--- src/commands/serverinfo.js
+++ src/commands/serverinfo.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder, MessageFlags, } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -23,6 +23,6 @@ module.exports = {
         { name: "📅 Criado em", value: `<t:${Math.floor(g.createdTimestamp / 1000)}:F>`, inline: true }
       );
 
-    await interaction.reply({ embeds: [embed], ephemeral: true });
+    await interaction.reply({ embeds: [embed], flags: MessageFlags.Ephemeral });
   }
 };
